e8c28b345eb73e706f9990f141e63dd15e7edaef
flo-mic <florianmichel@hotmail.de>
2021-01-11 17:35:07 +0100

    perform change to check update process

--- src/modules/check_openhab_config_updates.jsx
+++ src/modules/check_openhab_config_updates.jsx
@@ -49,6 +49,7 @@ export default class CheckOpenHABCockpitUpdates extends React.Component {
                 rm -r /opt/openhab-cockpit
                 git clone https://github.com/flo-mic/openHAB-cockpit.git /opt/openhab-cockpit
                 chmod +x src/scripts/*.sh
+                mkdir -p /usr/share/cockpit/openhab
                 cp -r dist/* /usr/share/cockpit/openhab
                 `, [], {
                 superuser: "require",
